6c925b415941b6be82bf689b3cd2f229b3c1d7e4
@uehaj <uehaj@jggug.org>
2017-12-23 21:58:04 +0900

    change title

--- client/src/resources/authors.js
+++ client/src/resources/authors.js
@@ -29,7 +29,7 @@ export const AuthorList = props => (
 );
 
 const AuthorTitle = ({ record }) => {
-  return <span>Author {record ? `"${record.title}"` : ''}</span>;
+  return <span>Author {record ? `"${record.name}"` : ''}</span>;
 };
 
 export const AuthorEdit = props => (
